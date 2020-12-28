ea489febd2be4a2046519325d5df60ba206c53b9
Kristof Jozsef Varga <kvarga@cloudera.com>
2020-12-28 13:56:12 +0100

    Revert "mend"
    This reverts commit 0fffb24335f936ef71e9746f918bef43b223d5af.

--- ql/src/java/org/apache/hadoop/hive/ql/txn/compactor/Initiator.java
+++ ql/src/java/org/apache/hadoop/hive/ql/txn/compactor/Initiator.java
@@ -66,7 +66,6 @@ public class Initiator extends CompactorThread {
   static final private String COMPACTORTHRESHOLD_PREFIX = "compactorthreshold.";
 
   private long checkInterval;
-  private long prevStart = -1;
 
   @Override
   public void run() {
@@ -93,10 +92,6 @@ public class Initiator extends CompactorThread {
         try {
           handle = txnHandler.getMutexAPI().acquireLock(TxnStore.MUTEX_KEY.Initiator.name());
           startedAt = System.currentTimeMillis();
-
-          long compactionInterval = (prevStart < 0) ? prevStart : (startedAt - prevStart)/1000;
-          prevStart = startedAt;
-
           //todo: add method to only get current i.e. skip history - more efficient
           ShowCompactResponse currentCompactions = txnHandler.showCompact(new ShowCompactRequest());
 
